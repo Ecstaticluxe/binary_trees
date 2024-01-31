9bc1729e9bd9f9d0118aaf3ca217ec3ba97e1c0e
Essy <esthercikechukwu@gmail.com>
2024-01-31 19:16:26 +0000

    update

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -5,16 +5,14 @@
 /**
  * binary_tree_delete - deletes an entire binary tree
  * @tree: a ponter to the node of the tree
- *
+ * Description: if tree is NULL, do nothing
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return;
+	if (!tree)
 
-	}
+		return;
 
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
