eb1133aead6fc5c28aba73ffdad5cf49102fae7e
Essy <esthercikechukwu@gmail.com>
2024-01-31 04:09:36 +0000

    mandatory

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,31 +14,21 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 {
+	binary_tree_t *new;
 
-	if (parent == NULL)
-
-	{
+	if (!parent)
 		return (NULL);
-	}
 
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-
-	if (new_node == NULL)
-	{
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
 		return (NULL);
-	}
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = parent->left;
-	new_node->right = NULL;
-
-	if (parent->left != NULL)
-	{
-		parent->left->parent = new_node;
-	}
-
-	parent->left = new_node;
-
-	return (new_node);
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+	new->left = parent->left;
+	parent->left = new;
+	if (new->left)
+		new->left->parent = new;
+	return (new);
 }
