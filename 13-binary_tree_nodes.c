056397b704f4e1271bf1f67deb7e736271a17ec7
Essy <esthercikechukwu@gmail.com>
2024-02-01 08:47:01 +0000

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,9 +14,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
     if (!tree)
         return (0);
 
-    size_t count = 0;
-    count += binary_tree_nodes(tree->left);
-    count += binary_tree_nodes(tree->right);
 
     if (tree->left || tree->right)
         count++;
