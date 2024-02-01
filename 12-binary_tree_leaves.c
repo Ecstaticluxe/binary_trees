86994667ff7e6558a16f1632adbef5a101e1cb1d
Essy <esthercikechukwu@gmail.com>
2024-02-01 08:33:40 +0000

    12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+#include <stddef.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	else if (!tree->left && !tree->right)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+
+}
