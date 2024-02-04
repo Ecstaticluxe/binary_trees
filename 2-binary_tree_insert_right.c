f917567d95898024dbcf0bd24530d93ff993a1d2
Essy <esthercikechukwu@gmail.com>
2024-02-04 12:47:08 +0000

    edit

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -27,10 +27,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = parent->right;
-	parent->right = new_node;
 
-		if (new_node->right)
-			new_node->parent->right = new_node;
+	parent->right = new_node;
+	
+	if (new_node->right)
+		new_node->parent->right = new_node;
 
 	return (new_node);
 }
