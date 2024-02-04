5661dd96b87cd07f80da85b97b9b995e7109cb21
Essy <esthercikechukwu@gmail.com>
2024-02-04 12:54:59 +0000

    7

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -19,6 +19,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	}
 
 	binary_tree_postorder(tree->left, func);
+	funct(tree->n)
 	binary_tree_postorder(tree->right, func);
 
 }
