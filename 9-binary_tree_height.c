ee61e07d1fbf5fe8a7c19e58a74b522c932ef4a1
Essy <esthercikechukwu@gmail.com>
2024-02-04 13:24:43 +0000

    7

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,12 +10,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height_l = 0;
+	size_t height_r = 0;
+
 	if (!tree)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-
-	 return (1 + ((left_height > right_height) ? left_height : right_height));
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
