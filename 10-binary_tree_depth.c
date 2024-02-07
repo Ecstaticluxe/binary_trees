bba0d4b6923d6b242217549838e179dd460b51db
Essy <esthercikechukwu@gmail.com>
2024-02-07 13:32:48 +0000

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,17 +7,18 @@
  * @tree: is a pointer to the node to measure the depth
  * Return: If tree is NULL, your function must return 0
  */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth = 0;
+
 	if (!tree)
 		return (0);
 
-	size_t depth = 0;
-
-	while (tree->parent != NULL)
+	while (tree->parent)
 	{
-	tree = tree->parent;
-	depth++;
+		depth++;
+		tree = tree->parent;
 	}
 
 	return (depth);
