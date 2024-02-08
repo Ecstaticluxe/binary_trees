bd24e579a5f389b4dd6f53a34cbb0032e38e1c2e
Essy <esthercikechukwu@gmail.com>
2024-02-08 17:35:13 +0000

    al

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: is a pointer to the first node
+ * @second: is a pointer to the second node
+ * Return: NULL if no common ancestor was found
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	const binary_tree_t *temp1 = first;
+	const binary_tree_t *temp2 = second;
+
+
+	while (temp1 != NULL)
+{
+	temp1 = temp1->parent;
+	temp2 = second;
+
+	while (temp2 != NULL)
+	{
+		if (temp1 == temp2)
+		{
+		return ((binary_tree_t *)temp1);
+		}
+		temp2 = temp2->parent;
+	}
+}
+	return (NULL);
+}
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,25 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: a pointer to the root node of the tree to check
- * Return: 0 if tree is NULL
+ * @tree: pointer to the root node of the tree to check
+ * Return: 0 if tree is NULL, 1 if perfect, 0 otherwise
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height = 0;
-	size_t nodes = 0;
-	size_t power = 0;
+    int height;
 
-	if (!tree)
-		return (0);
+    if (!tree)
+        return (0);
 
-	if (!tree->right && !tree->left)
-		return (1);
+    height = binary_tree_height(tree);
+    nodes = (1 << height) - 1;
 
-	height = binary_tree_height(tree);
-	nodes = binary_tree_size(tree);
-
-	power = (size_t)_pow_recursion(2, height + 1);
-	return (power - 1 == nodes);
+    return (tree);
 }
